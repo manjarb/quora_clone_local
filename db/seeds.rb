65e0aa0f6a972e98489e2eb3de4ef4c782907efa
manjarb <manjarb4@gmail.com>
2016-02-01 15:51:15 +0800

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@ require 'faker'
 
 class TaskSeed
 	def self.faker
-		# 20.times do 
-		# 	Question.create(
-		# 		:content => Faker::Lorem.sentence(200, true), 
-		# 		:headline => Faker::Lorem.sentence,
-		# 		:user_id => Faker::Number.between(1, 10)
-		# 		) 
-		# end
+		20.times do 
+			Question.create(
+				:content => Faker::Lorem.sentence(200, true), 
+				:headline => Faker::Lorem.sentence,
+				:user_id => Faker::Number.between(1, 10)
+				) 
+		end
 
-		# 40.times do 
-		# 	Answer.create(
-		# 		:content => Faker::Lorem.sentence(20, true), 
-		# 		:headline => Faker::Lorem.sentence,
-		# 		:user_id => Faker::Number.between(1, 10),
-		# 		:question_id => Faker::Number.between(1, 10)
-		# 		) 
-		# end
+		40.times do 
+			Answer.create(
+				:content => Faker::Lorem.sentence(20, true), 
+				:headline => Faker::Lorem.sentence,
+				:user_id => Faker::Number.between(1, 10),
+				:question_id => Faker::Number.between(1, 10)
+				) 
+		end
 
 		20.times do
 			User.create(
@@ -30,14 +30,14 @@ class TaskSeed
 				) 
 		end
 
-		# vote_type_arr = ['up','down'] 
+		vote_type_arr = ['up','down'] 
 
-		# 50.times do
-		# 	QuestionVote.create(
-		# 		:type => vote_type_arr[rand(0..1)],
-		# 		:question_id => Faker::Number.between(1, 20)
-		# 		) 
-		# end
+		50.times do
+			QuestionVote.create(
+				:type => vote_type_arr[rand(0..1)],
+				:question_id => Faker::Number.between(1, 20)
+				) 
+		end
 	end
 end
 
