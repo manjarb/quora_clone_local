8df845e925c993c8e2e79bde9f80d6e03dd21d83
manjarb <manjarb4@gmail.com>
2016-02-01 16:02:30 +0800

    aa

--- db/migrate/20160201113814_drop_vote_table.rb
+++ db/migrate/20160201113814_drop_vote_table.rb
@@ -1,6 +1,6 @@
 class DropVoteTable < ActiveRecord::Migration
 	def change
-		# drop_table :question_votes
-		# drop_table :answer_votes
+		drop_table :question_votes
+		drop_table :answer_votes
 	end
 end
